ccb92194785e28b752d594cd3104686ebef6776b
susmoysaha6 <susmoysaha6@gmail.com>
2022-08-22 14:17:57 +0600

    event handler adder for calculate button

--- js/app.js
+++ js/app.js
@@ -21,3 +21,28 @@ for (let i = 0; i < items.length; i++) {
         // event.target.removeEventListener();
     })
 }
+// 
+function getElementByInputId(inputId) {
+    const inputElement = document.getElementById(inputId);
+    const inputString = inputElement.value;
+    const inputNumber = parseFloat(inputString);
+    inputElement.value = '';
+    return inputNumber;
+
+}
+// event handler for calculate button
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const element = document.getElementById("player-list");
+    let totalPlayer = element.childNodes.length - 1;
+    const perPlayerBudget = getElementByInputId('budget-per-player')
+    const playerExpenceField = document.getElementById('player-expence');
+
+    if (isNaN(perPlayerBudget) || perPlayerBudget < 0) {
+        alert('Enter valid number');
+        return;
+    }
+    const totalPlayerExpence = totalPlayer * perPlayerBudget;
+    playerExpenceField.innerText = totalPlayerExpence;
+
+})
